b54a34a63f5bbe1463eabbd80e4965e280d43bfd
ARNAUSOLANSIVAZQUEZ <arnau.solans01@estudiant.upf.edu>
2024-02-18 13:15:15 +0100

    Benchmark lines added

--- lab1/src/main/java/edu/upf/TwitterFilter.java
+++ lab1/src/main/java/edu/upf/TwitterFilter.java
@@ -4,11 +4,14 @@ import edu.upf.filter.FileLanguageFilter;
 import edu.upf.uploader.S3Uploader;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 
 public class TwitterFilter {
     public static void main( String[] args ) throws IOException {
+        LocalTime start = LocalTime.now();
         List<String> argsList = Arrays.asList(args);
         String language = argsList.get(0);
         String outputFile = argsList.get(1);
@@ -19,8 +22,14 @@ public class TwitterFilter {
             final FileLanguageFilter filter = new FileLanguageFilter(inputFile, outputFile);
             filter.filterLanguage(language);
         }
-
+        System.out.println("Uploading processed file.");
         final S3Uploader uploader = new S3Uploader(bucket, "tweets", "default");
         uploader.upload(Arrays.asList(outputFile));
+        LocalTime end = LocalTime.now();
+        Duration elapsed = Duration.between(start, end);
+        long secondsElapsed = elapsed.getSeconds();
+        secondsElapsed = Math.abs(secondsElapsed);
+        System.out.println(String.format("\n Total time elapsed Twitter Filtering:  %d h %02d m %02d s" , secondsElapsed / 3600, (secondsElapsed % 3600) / 60, (secondsElapsed % 60)));
+
     }
 }
--- lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
+++ lab1/src/main/java/edu/upf/filter/FileLanguageFilter.java
@@ -1,12 +1,15 @@
 package edu.upf.filter;
 
 import edu.upf.parser.SimplifiedTweet;
+import jdk.vm.ci.meta.Local;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Optional;
 
 
@@ -23,6 +26,8 @@ public class FileLanguageFilter implements LanguageFilter {
     // Implementation of filterLanguage method
     @Override
     public void filterLanguage(String language) throws IOException {
+        int tweetcount = 0;
+        LocalTime filterStart = LocalTime.now();
         // We use a reader buffer to read lines from input file and a writer buffer to write to the output file
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
@@ -37,6 +42,7 @@ public class FileLanguageFilter implements LanguageFilter {
                     if (tweet.getLanguage().equals(language)) {
                         writer.write(line);
                         writer.newLine();
+                        tweetcount += 1;
                     }
                 }
             }
@@ -44,5 +50,11 @@ public class FileLanguageFilter implements LanguageFilter {
         } catch (IOException e) {
             throw new IOException("Error reading/writing files", e);
         }
+        LocalTime filterEnd = LocalTime.now();
+        Duration filterElapsed = Duration.between(filterStart, filterEnd);
+        long secondsElapsedFilter = filterElapsed.getSeconds();
+        secondsElapsedFilter = Math.abs(secondsElapsedFilter);
+        System.out.println("\nFile processed: "+ inputFile +"\nLanguage filtered: " +language + "\nNumber of tweets filtered: " + tweetcount);
+        System.out.println(String.format("Time elapsed: %d h %02d m %02d s\n", secondsElapsedFilter / 3600, (secondsElapsedFilter % 3600) / 60, (secondsElapsedFilter % 60)));
     }
 }
